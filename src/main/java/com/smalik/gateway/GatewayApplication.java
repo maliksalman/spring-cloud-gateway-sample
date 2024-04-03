12332d487a3d147d0c7822638b0db990066bb57a
Salman Malik <smalik@pivotal.io>
2024-04-03 18:10:06 -0400

    Upgrading to MVC server based gateway and boot 3.2.4

--- src/main/java/com/smalik/gateway/GatewayApplication.java
+++ src/main/java/com/smalik/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.smalik.initiator;
+package com.smalik.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +9,5 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
 }
